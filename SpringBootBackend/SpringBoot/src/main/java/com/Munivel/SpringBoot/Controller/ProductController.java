4b92cd2db62bad132d954f96954d571a252e544d
munivel <munivel.javadev@gmail.com>
2025-04-23 14:27:40 +0530

    Second Commit

--- SpringBootBackend/SpringBoot/src/main/java/com/Munivel/SpringBoot/Controller/ProductController.java
+++ SpringBootBackend/SpringBoot/src/main/java/com/Munivel/SpringBoot/Controller/ProductController.java
@@ -1,5 +1,98 @@
-package com.Munivel.SpringBoot.Controller;
 
+//
+//@RestController
+//@CrossOrigin
+//@RequestMapping("/api")
+//public class ProductController {
+//    @Autowired
+//    private ProductService service;
+//    @RequestMapping("/")
+//    public String greet(){
+//        return "hii";
+//    }
+//    //returning an data as well as status
+//    @GetMapping("/products")
+//    public ResponseEntity<List<Product>> productList(){ // bascially we are sending an list of product but we can send asResponseEntity
+//        return new ResponseEntity<>(service.getallproducts(), HttpStatus.OK);
+//    }
+//    @GetMapping("/product/{id}")
+//    public ResponseEntity<Product> getProduct(@PathVariable int id){
+//
+//        Product product=service.getProduct(id);
+//        if(product != null)
+//            return new ResponseEntity<>(product,HttpStatus.OK);
+//        else
+//          return   new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//
+//        // in this code the prodct from the client and it is fetched from the db and checking if the product exist it will send the product
+//        // if it is not exit we have an constructoru not found status will be sended
+//    }
+//
+//
+//
+//
+//
+//    //? meands any type of data not sure might return an data or exception
+//    // @requestbody accept whole object from json
+//    // @requestpart help to dive and gettting an splitter one part and two part
+//    // this method for adding an product
+//
+//    @PostMapping("/product")// if it is  normal product without an any image you can use the RequestBody if it is an image
+//  public ResponseEntity<?> addproducts(@RequestPart  Product  product,
+//                                      @RequestPart MultipartFile imagefile){ // we are geeting an product object if also give an image means
+//        try {
+//            Product product1 =service.addProduct(product, imagefile);
+//            return new ResponseEntity<>(product1,HttpStatus.CREATED);// need to check actually getting it stored
+//        }catch (Exception e){
+//            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR
+//            );
+//        }
+//    }
+//    @GetMapping("/product/{productId}/image")
+//    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+//        Product product=service.getProduct(productId);
+//        //getting an image
+//        byte[] imagefile=product.getImageDate();
+//        return ResponseEntity.ok()
+//                .contentType(MediaType.valueOf(product.getImageType())).body(imagefile);
+//    }
+//    @PutMapping("/product/{id}")
+//    public ResponseEntity<String> update(@PathVariable int id,@RequestPart  Product  product,
+//                                         @RequestPart MultipartFile imagefile){
+//        Product product1= null;
+//        try {
+//            product1 = service.updateproduct(id,product,imagefile);
+//        } catch (IOException e) {
+//            return new ResponseEntity<>("Failed to update",HttpStatus.BAD_REQUEST);
+//        }
+//        if(product1 != null){
+//            return new ResponseEntity<>("updated",HttpStatus.OK);
+//        }else{
+//            return new ResponseEntity<>("Failed to update",HttpStatus.BAD_REQUEST);
+//        }
+//
+//    }
+//    @DeleteMapping("/product/{id}")
+//    public ResponseEntity<String> delect(@PathVariable int id){
+//        Product product=service.getProduct(id);// check first the product is present or not
+//        if(product != null){
+//            service.delete(id);
+//            return new ResponseEntity<>("Successfully deleted",HttpStatus.OK);
+//        }
+//        else{
+//            return new ResponseEntity<>("Product not found",HttpStatus.NOT_FOUND);
+//        }
+//
+//    }
+//    @GetMapping("/products/search")// use requestparam to get an string data to map
+//    public  ResponseEntity<List<Product>> searchProduct(@RequestParam String keyword){
+//        List<Product> products=service.searchProduct(keyword);
+//        return  new ResponseEntity<>(products,HttpStatus.OK);
+//
+//    }
+//}
+
+package com.Munivel.SpringBoot.Controller;
 import com.Munivel.SpringBoot.Model.Product;
 import com.Munivel.SpringBoot.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,95 +104,90 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-
 @RestController
 @CrossOrigin
 @RequestMapping("/api")
-public class ProductController {
+public class ProductController {// with the help of hte responentity we can also send the status
+
     @Autowired
     private ProductService service;
-    @RequestMapping("/")
-    public String greet(){
-        return "hii";
+
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
-    //returning an data as well as status
-    @GetMapping("/products")// with the help of hte responentity we can also send the status
-    public ResponseEntity<List<Product>> productList(){ // bascially we are sending an list of product but we can send asResponseEntity
-        return new ResponseEntity<>(service.getallproducts(), HttpStatus.OK);
-    }
+
     @GetMapping("/product/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable int id){
 
-        Product product=service.getProduct(id);
-        if(product != null)
-            return new ResponseEntity<>(product,HttpStatus.OK);
-        else
-          return   new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        Product product = service.getProductById(id);
 
-        // in this code the prodct from the client and it is fetched from the db and checking if the product exist it will send the product
-        // if it is not exit we have an constructoru not found status will be sended
+        if(product != null)
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-
-
-
-
-    //? meands any type of data not sure might return an data or exception
-    // @requestbody accept whole object from json
-    // @requestpart help to dive and gettting an splitter one part and two part
-    // this method for adding an product
-
-    @PostMapping("/product")// if it is  normal product without an any image you can use the RequestBody if it is an image
-  public ResponseEntity<?> addproducts(@RequestPart  Product  product,
-                                      @RequestPart MultipartFile imagefile){ // we are geeting an product object if also give an image means
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile){
         try {
-            Product product1 =service.addProduct(product, imagefile);
-            return new ResponseEntity<>(product1,HttpStatus.CREATED);// need to check actually getting it stored
-        }catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR
-            );
+            System.out.println(product);
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
     @GetMapping("/product/{productId}/image")
     public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
-        Product product=service.getProduct(productId);
-        //getting an image
-        byte[] imagefile=product.getImageDate();
+
+        Product product = service.getProductById(productId);
+        byte[] imageFile = product.getImageDate();
+
         return ResponseEntity.ok()
-                .contentType(MediaType.valueOf(product.getImageType())).body(imagefile);
+                .contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile);
+
     }
+
     @PutMapping("/product/{id}")
-    public ResponseEntity<String> update(@PathVariable int id,@RequestPart  Product  product,
-                                         @RequestPart MultipartFile imagefile){
-        Product product1= null;
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile){
+        Product product1 = null;
         try {
-            product1 = service.updateproduct(id,product,imagefile);
+            product1 = service.updateProduct(id, product, imageFile);
         } catch (IOException e) {
-            return new ResponseEntity<>("Failed to update",HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
         }
-        if(product1 != null){
-            return new ResponseEntity<>("updated",HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>("Failed to update",HttpStatus.BAD_REQUEST);
-        }
-
+        if(product1 != null)
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
     }
+
+
     @DeleteMapping("/product/{id}")
-    public ResponseEntity<String> delect(@PathVariable int id){
-        Product product=service.getProduct(id);// check first the product is present or not
-        if(product != null){
-            service.delete(id);
-            return new ResponseEntity<>("Successfully deleted",HttpStatus.OK);
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Product product = service.getProductById(id);
+        if(product != null) {
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);
         }
-        else{
-            return new ResponseEntity<>("Product not found",HttpStatus.NOT_FOUND);
-        }
+        else
+            return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
 
     }
-    @GetMapping("/products/search")// use requestparam to get an string data to map
-    public  ResponseEntity<List<Product>> searchProduct(@RequestParam String keyword){
-        List<Product> products=service.searchProduct(keyword);
-        return  new ResponseEntity<>(products,HttpStatus.OK);
 
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword){
+        List<Product> products = service.searchProducts(keyword);
+        System.out.println("searching with " + keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
+
+
 }
--- SpringBootBackend/SpringBoot/src/main/java/com/Munivel/SpringBoot/Service/ProductService.java
+++ SpringBootBackend/SpringBoot/src/main/java/com/Munivel/SpringBoot/Service/ProductService.java
@@ -9,48 +9,87 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+//
+//@Service
+//public class ProductService {
+//    @Autowired
+//    ProductInterface productInterface;
+//
+//    public List<Product> getallproducts() {
+//        return productInterface.findAll();
+//    }
+//
+//// option return means that the particular id there is no any product present in the database it will return nulll
+//
+//
+//
+//
+//    public Product getProduct(int id) {
+//        return productInterface.findById(id).orElse(new Product());
+//    }
+//
+//
+//
+//    public Product addProduct(Product product, MultipartFile imagefile) throws IOException {
+//        // we get an image and convert into the byte formate to store in the data base also we need set an image name type and an image
+//        product.setImageName(imagefile.getOriginalFilename());
+//        product.setImageType(imagefile.getContentType());
+//        product.setImageDate(imagefile.getBytes());// to convert an image to byte we have an method called getBytes();
+//        return  productInterface.save(product);// in this only we are sending an product not an image
+//    }
+//
+//
+//    public Product updateproduct(int id, Product product, MultipartFile imagefile) throws IOException {
+//        product.setImageDate(imagefile.getBytes());
+//        product.setImageName(imagefile.getOriginalFilename());
+//        product.setImageType(imagefile.getContentType());
+//        return productInterface.save(product);
+//    }
+//
+//    public void delete(int id) {
+//        productInterface.deleteById(id);
+//    }
+//
+//    public List<Product> searchProduct(String keyword) {
+//        return productInterface.searchProduct(keyword);
+//    }
+//}
 
 @Service
 public class ProductService {
+
     @Autowired
-    ProductInterface productInterface;
+    private ProductInterface repo;
 
-    public List<Product> getallproducts() {
-        return productInterface.findAll();
+    public List<Product> getAllProducts() {
+
+        return repo.findAll();
+
     }
 
-// option return means that the particular id there is no any product present in the database it will return nulll
-
-
-
-
-    public Product getProduct(int id) {
-        return productInterface.findById(id).orElse(new Product());
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
     }
 
-
-
-    public Product addProduct(Product product, MultipartFile imagefile) throws IOException {
-        // we get an image and convert into the byte formate to store in the data base also we need set an image name type and an image
-        product.setImageName(imagefile.getOriginalFilename());
-        product.setImageType(imagefile.getContentType());
-        product.setImageDate(imagefile.getBytes());// to convert an image to byte we have an method called getBytes();
-        return  productInterface.save(product);// in this only we are sending an product not an image
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+        return repo.save(product);
     }
 
-
-    public Product updateproduct(int id, Product product, MultipartFile imagefile) throws IOException {
-        product.setImageDate(imagefile.getBytes());
-        product.setImageName(imagefile.getOriginalFilename());
-        product.setImageType(imagefile.getContentType());
-        return productInterface.save(product);
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageDate(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        return repo.save(product);
     }
 
-    public void delete(int id) {
-        productInterface.deleteById(id);
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
     }
 
-    public List<Product> searchProduct(String keyword) {
-        return productInterface.searchProduct(keyword);
+    public List<Product> searchProducts(String keyword) {
+        return repo.searchProduct(keyword);
     }
 }
